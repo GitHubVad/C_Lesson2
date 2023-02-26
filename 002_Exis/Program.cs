875885b6a173ad0a3e65f704b464dbf575c55625
GitHubVad <onwww@yandex.ru>
2023-02-26 13:57:47 +0300

    Запушил игнор

---
+++ 002_Exis/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB == numA * numA)
+Console.WriteLine("Второе число квадрат первого");
+else if (numA == numB * numB)
+Console.WriteLine("Первое число квадрат второго");
+else 
+Console.WriteLine("Числа не являются квадратами друг друга");
+
